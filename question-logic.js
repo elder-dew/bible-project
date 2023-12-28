bd91700e05acbc36c2b8fb6a900cebf92c09a5b7
elder-dew <caleb.dew@missionary.org>
2023-12-27 18:47:58 -0800

    Added in some functionality and did the buttons

---
+++ question-logic.js
@@ -0,0 +1,6 @@
+var buttons = document.getElementsByClassName("answer-button");
+var questionP = document.getElementById("question");
+
+function nextQuestion() {
+    questionP.innerText = "New question, bro!";
+}
