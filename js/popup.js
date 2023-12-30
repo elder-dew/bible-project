acbd33f84260782d8993f559ed995dba6b6027a6
elder-dew <caleb.dew@missionary.org>
2023-12-29 17:51:11 -0800

    Made popups for the institute and family history and stuff

---
+++ js/popup.js
@@ -0,0 +1,24 @@
+var familyHistoryPopup = document.getElementById('familyHistoryPopup');
+var familyHistoryButton = document.getElementById('familyHistory');
+
+var instituteButton = document.getElementById('institute');
+var institutePopup = document.getElementById('institutePopup')
+
+var churchButton = document.getElementById('church');
+var churchPopup = document.getElementById('churchPopup')
+
+familyHistoryButton.addEventListener('click', () => { OpenPopup(familyHistoryPopup) });
+instituteButton.addEventListener('click', () => { OpenPopup(institutePopup)});
+churchButton.addEventListener('click', () => { OpenPopup(churchPopup)});
+
+function OpenPopup(popup) {
+    //if (popup.classList.contains('open')) {
+    //    ClosePopup(popup);
+    //    return
+    //}
+    popup.classList.add('open');
+}
+
+function ClosePopup(popup) {
+    popup.classList.remove('open');
+}
